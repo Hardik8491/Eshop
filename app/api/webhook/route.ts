baf529ef5795465a5aca0ddc0ec9851a4a5a6c6b
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-08-27 13:13:55 +0530

    change and deboug webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,106 +1,56 @@
-import * as admin from "firebase-admin";
-import { buffer } from "micro";
-
+import Stripe from "stripe";
+import {buffer } from 'micro'
 import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest } from "next";
 
-const serviceAccount = require("../../../permissions.json");
-admin.initializeApp();
-const app = !admin.app.length
-  ? admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: "database_url",
-    })
-  : admin.app();
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const endpointSecret = "whsec_UPkg9LLymSKKzFC7J6ywpGnu6URnuxkZ";
+const stripe = new Stripe("sk_test_51NNGilSCNlaf5syugoxtZReJSAEYU1JIHZhAbQA8dtQfwXGENlz1vZSj7jZoTzlyWC7aaANOmfpxc9SkNDSDxm0s003tghmVjJ", {
+  apiVersion: "2022-11-15",
+});
 
-// const fulfillOrder = async (session: any) => {
-//   console.log(session);
-//   console.log("web hook working,Fulfill order ");
-//   return app
-//     .firestore()
-//     .collection("users")
-//     .doc(session.metadata.email)
-//     .collection("orders")
-//     .doc(session.id)
-//     .set({
-//       amount: session.amount_total / 100,
-//       amount_shipping: session.total_details.amount_shipping / 100,
-//       images: JSON.parse(session.metadata.images),
-//       timestamp: admin.firestore.FieldValue.serverTimestamp(),
-//     })
-//     .then(() => {
-//       console.log(`SUCCESS: Order ${session.id} had been added to the  DB`);
-//       console.log("error is definee");
-//     });
+const webhookSecret = process.env.STRIPE_SIGNING_SECRET;
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
 // };
-// // // export default async (req, res) => {
-// //   if (req.method === "POST") {
-// //     const requestBuffer = await buffer(req);
-// //     const payload = requestBuffer.toString();
-//     // const sig = req.headers["stripe-signature"];
-//     // let event;
 
-// //     try {
-// //       event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-// //       //     //     let paymentIntent = null;
-// //       //     // switch (event.type) {
-// //       //     //   case "payment_intent.created":
-// //       //     //     paymentIntent = event.data.object;
-// //       //     //     functions.logger.log("Payment Intent Created", paymentIntent.id);
-// //       //     //     break;
-// //       //     //   case "payment_intent.succeeded":
-// //       //     //     paymentIntent = event.data.object;
-// //       //     //     functions.logger.log("Payment Intent Succeeded", paymentIntent.id);
-// //       //     //     break;
-// //       //     //   case "payment_intent.canceled":
-// //       //     //     paymentIntent = event.data.object;
-// //       //     //     functions.logger.log("Payment Intent Cancelled", paymentIntent.id);
-// //       //     //     break;
-// //       //     //   default:
-// //       //     //     functions.logger.log("Unhandled event type", event.type);
-// //       //     //     break;
-// //       // }
-// //     } catch (error) {
-// //       console.log("ERROR", err.message);
-// //       return res.status(400).send(`Webhook error: ${err.message}`);
-// //     }
-// //     //handle checkout
-// //     console.log("webhook is ghaitiya line no:72");
-// //     if (event.type === "checkout.session.completed") {
-// //       const session = event.date.object;
-// //       //Fulfill the order
-// //       return fulfillOrder(session)
-// //         .then(() => res.status(200))
-// //         .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
-// //     }
-// //   }
-// // };
-
-// // export const config = {
-// //   api: {
-// //     bodyParser: false,
-// //     externalResolver: true,
-// //   },
-// // };
-
-export async function POST(req: any) {
-  const requestBuffer = await buffer(req);
-  const payload = requestBuffer.toString();
-  const sig = req.headers.get("stripe-signature");
+export async function POST(req:any) {
+  const buf =  await buffer(req);
+  const payload = buf.toString();
+  const sig = req.headers["stripe-signature"];
   let event;
+  
+  console.log(req, buf);
+
+  // req=await req.json()
+  // const buf = await buffer(req);
+
+
 
   try {
-    event = await stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-  } catch (error: any) {
-    console.log("ERROR", error.message);
-    return new Error(`Webhook error:${error}`);
+    event = stripe.webhooks.constructEvent(buf, sig,webhookSecret!);
+  } catch (err: any) {
+    console.error(`Webhook signature verification failed: ${err.message}`);
+    // return res
+    //   .status(400)
+    //   .send(`Webhook signature verification failed: ${err.message}`);
+    return new NextResponse("WEBhook signature failsed", { status: 400 });
   }
-  //handle checkout
-  console.log("webhook is ghaitiya line no:72");
-  if (event.type === "checkout.session.completed") {
-    const session = event.date.object;
+
+  switch (event.type) {
+    case "checkout.session.completed":
+      const session = event.data.object;
+
+      // Handle post-payment actions here
+      break;
+
+    // Add other event types to handle as needed
+
+    default:
+      console.warn(`Unhandled event type: ${event.type}`);
   }
-  return new NextResponse("Success", { status: 200 });
+
+  return new NextResponse("OK", { status: 200 });
 }
