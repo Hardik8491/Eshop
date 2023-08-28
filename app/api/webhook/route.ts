ef9b1c28782e983801a38c5b0edf2da56935a6a4
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-08-28 09:41:23 +0530

    change and deboug webchang

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,7 +3,6 @@
 import { Stripe } from "stripe";
 import { headers } from "next/headers";
 
-
 // import getRawBody from 'raw-body'
 import { buffer } from "micro";
 
@@ -14,7 +13,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 const fulfillOrder = async (session: any) => {
-  console.log(session);
+  // console.log(session);
 };
 
 // export const config = {
@@ -28,14 +27,11 @@ export async function POST(request: any) {
   const signature = headers().get("Stripe-Signature") ?? "";
   // const rawBody= await getRawBody(request);
   const body = await request.text();
-  
 
   // const stripePayload = (request as any).rawBody || request.body;
 
-  
-
-  const STRIPE_SIGNING_SECRET =
-    "whsec_00eff49e647740b929bca4751d8712ad08d6285bf6f123b24c48e0fd6c2cca66";
+  const STRIPE_SIGNING_SECRET =process.env.STRIPE_SIGNING_SECRET!;
+    // "whsec_00eff49e647740b929bca4751d8712ad08d6285bf6f123b24c48e0fd6c2cca66";
   let event: Stripe.Event;
 
   try {
@@ -44,53 +40,17 @@ export async function POST(request: any) {
       signature,
       STRIPE_SIGNING_SECRET
     );
-    console.log(event);
-    return NextResponse.json({ message: "This Worked",success:true });
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+      // console.log("session", session);
+    }
+    // console.log(event);
+    return NextResponse.json({ message: "This Worked", success: true });
+
+
   } catch (err: any) {
-    return new NextResponse(err, { status: 500 });
-    
-    console.log(err);
+    return new NextResponse(err, { status: 500 }), console.log(err);
   }
-  if (event.type === "checkout.session.completed") {
-    const session = event.data.object;
-    console.log("session",session)
-  }
+ 
 }
-// export const config={
-//   api:{
-//     bodyParser:false,
-//     externalResolver:true
-//   },
-// }
-
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import { NextRequest, NextResponse } from 'next/server';
-// import { Stripe } from 'stripe';
-
-// const stripe = new Stripe(process.env.STRIPE_SIGNING_SECRET!, {
-//   apiVersion: "2022-11-15",
-// });
-
-// export  async function POST(req: NextApiRequest, res: NextApiResponse) {
-//   try {
-//     const rawBody = await buffer(req);
-
-//     const sig = req.headers['stripe-signature'] as string;
-//     const event = stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_SECRET_KEY!);
-
-//     // Handle the event here...
-
-//   } catch (error) {
-//     console.error(error);
-
-//   }
-// }
-
-// // Buffer the raw request body
-// const buffer = async (req: NextApiRequest) => {
-//   const chunks = [];
-//   for await (const chunk of req) {
-//     chunks.push(chunk);
-//   }
-//   return Buffer.concat(chunks);
-// };
